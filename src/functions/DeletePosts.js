ccacfb96ba21c4fef66c82fab84957e67b5f3396
lukalomidze <lomidzeluka.w@gmail.com>
2024-06-17 16:59:08 +0400

    Updated to also delete image blobs

--- src/functions/DeletePosts.js
+++ src/functions/DeletePosts.js
@@ -1,17 +1,33 @@
 const { app } = require('@azure/functions');
 const { CosmosClient } = require('@azure/cosmos');
+const { BlobServiceClient } = require("@azure/storage-blob");
 
 const endpoint = process.env['AZURE_COSMOSDB_ENDPOINT'];
 const key = process.env['AZURE_COSMOSDB_ACCOUNT_KEY'];
 const databaseName = process.env['AZURE_COSMOSDB_DATABASE_NAME'];
-const containerName = process.env['AZURE_COSMOSDB_CONTAINER_NAME'];
+const cosmosContainerName = process.env['AZURE_COSMOSDB_CONTAINER_NAME'];
+const blobStorageConnectionString = process.env['AZURE_BLOBSTORAGE_CONNECTION_STRING'];
+const blobContainerName = process.env['AZURE_BLOBSTORAGE_CONTAINER_NAME'];
 
 app.timer('DeletePosts', {
-    schedule: '0 0 * * * *',
-    handler: async () => {
-        const client = new CosmosClient({ endpoint, key });
-        const container = client.database(databaseName).container(containerName);
+    schedule: '0 0 0 * * *',
+    handler: async (myTimer, context) => {
+        const cosmosClient = new CosmosClient({ endpoint, key });
+        const container = cosmosClient.database(databaseName).container(cosmosContainerName);
 
-        await container.delete()
+        try {
+            await container.delete();
+        } catch (err) {
+            context.error('Error deleting cosmosdb container:', err);
+        }
+
+        const blobClient = BlobServiceClient.fromConnectionString(blobStorageConnectionString);
+        const blobContainerClient = blobClient.getContainerClient(blobContainerName);
+
+        try {
+            await blobContainerClient.delete();
+        } catch (err) {
+            context.error('Error deleting blob container:', err);
+        }
     }
 });
