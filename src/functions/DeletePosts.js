fde6f6257e06f14bcd254e57c648a5e4726adc8d
lukalomidze <lomidzeluka.w@gmail.com>
2024-06-17 19:44:02 +0400

    Updated to delete items instead of containers

--- src/functions/DeletePosts.js
+++ src/functions/DeletePosts.js
@@ -1,33 +1,41 @@
-const { app } = require('@azure/functions');
-const { CosmosClient } = require('@azure/cosmos');
-const { BlobServiceClient } = require("@azure/storage-blob");
+const { app } = require('@azure/functions')
+const { CosmosClient } = require('@azure/cosmos')
+const { BlobServiceClient } = require("@azure/storage-blob")
 
-const endpoint = process.env['AZURE_COSMOSDB_ENDPOINT'];
-const key = process.env['AZURE_COSMOSDB_ACCOUNT_KEY'];
-const databaseName = process.env['AZURE_COSMOSDB_DATABASE_NAME'];
-const cosmosContainerName = process.env['AZURE_COSMOSDB_CONTAINER_NAME'];
-const blobStorageConnectionString = process.env['AZURE_BLOBSTORAGE_CONNECTION_STRING'];
-const blobContainerName = process.env['AZURE_BLOBSTORAGE_CONTAINER_NAME'];
+const endpoint = process.env['AZURE_COSMOSDB_ENDPOINT']
+const key = process.env['AZURE_COSMOSDB_ACCOUNT_KEY']
+const databaseName = process.env['AZURE_COSMOSDB_DATABASE_NAME']
+const cosmosContainerName = process.env['AZURE_COSMOSDB_CONTAINER_NAME']
+const blobStorageConnectionString = process.env['AZURE_BLOBSTORAGE_CONNECTION_STRING']
+const blobContainerName = process.env['AZURE_BLOBSTORAGE_CONTAINER_NAME']
 
 app.timer('DeletePosts', {
     schedule: '0 0 0 * * *',
     handler: async (myTimer, context) => {
-        const cosmosClient = new CosmosClient({ endpoint, key });
-        const container = cosmosClient.database(databaseName).container(cosmosContainerName);
+        const cosmosContainer = new CosmosClient({ endpoint, key })
+            .database(databaseName)
+        .container(cosmosContainerName)
 
         try {
-            await container.delete();
+            const { resources: documents } = await cosmosContainer.items.readAll().fetchAll()
+
+            for (const document of documents) {
+                await cosmosContainer.item(document.id, document.partitionKey).delete()
+            }
         } catch (err) {
-            context.error('Error deleting cosmosdb container:', err);
+            context.error('Error deleting cosmosdb container items: ', err)
         }
 
-        const blobClient = BlobServiceClient.fromConnectionString(blobStorageConnectionString);
-        const blobContainerClient = blobClient.getContainerClient(blobContainerName);
+        const blobContainerClient = BlobServiceClient
+            .fromConnectionString(blobStorageConnectionString)
+        .getContainerClient(blobContainerName)
 
         try {
-            await blobContainerClient.delete();
+            for await (const blob of blobContainerClient.listBlobsFlat()) {
+                await blobContainerClient.deleteBlob(blob.name)
+            }
         } catch (err) {
-            context.error('Error deleting blob container:', err);
+            context.error('Error deleting blob container items: ', err)
         }
     }
-});
+})
